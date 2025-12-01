7b3e852a851ddaa8db66910a82be9c8210a24f3e
Peterson Fontinhas <petersonfontinhas@gmail.com>
2025-12-01 13:38:59 -0300

    estrutura de formulário e processo avaliativo adicionada

--- sistemaavaliacao/src/main/java/br/ufpr/sistemaavaliacao/model/Formulario.java
+++ sistemaavaliacao/src/main/java/br/ufpr/sistemaavaliacao/model/Formulario.java
@@ -2,6 +2,10 @@ package br.ufpr.sistemaavaliacao.model;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Formulario {
     private int id; 
     private int processoAvaliativoId; 
@@ -10,18 +14,4 @@ public class Formulario {
     private String instrucoes; 
     private List<Questao> questoes; 
 
-    // Construtores e Getters/Setters (Omitidos para brevidade)
-    public Formulario() {}
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public int getProcessoAvaliativoId() { return processoAvaliativoId; }
-    public void setProcessoAvaliativoId(int processoAvaliativoId) { this.processoAvaliativoId = processoAvaliativoId; }
-    public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
-    public boolean isAnonimo() { return isAnonimo; }
-    public void setAnonimo(boolean isAnonimo) { this.isAnonimo = isAnonimo; }
-    public String getInstrucoes() { return instrucoes; }
-    public void setInstrucoes(String instrucoes) { this.instrucoes = instrucoes; }
-    public List<Questao> getQuestoes() { return questoes; }
-    public void setQuestoes(List<Questao> questoes) { this.questoes = questoes; }
 }
